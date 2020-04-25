5217060b79a9a45be2de17d5cc2a606f776c5896
Miftakhuddin Falaki <60594222+kikuKeii@users.noreply.github.com>
2020-04-26 01:15:28 +0700

    Update script.js
    Delete quote

--- script/script.js
+++ script/script.js
@@ -81,35 +81,6 @@
 			}
 			startTime();
 
-			const quotes = [
-			    {
-					"quote": "Yaudah daripada kalian depresi kalian nge si(m)p in aku aja",
-					"source": "Live Mama Moona"
-				  },
-				  {
-					"quote": "Bukan Moonaroh",
-					"source": "Live Perkenalan"
-				  },
-				  {
-					"quote": "Jelek zombie jelek",
-					"source": "Moona Hoshinova"
-				  },
-				 {
-					"quote": "Moona Suka Melon",
-					"source": "Moona Hoshinova"
-				  },
-				{
-					"quote": "Ohh.. Melon Yang itu,..",
-					"source": "Moona Hoshinova"
-				  },
-			]
-			setInterval(randomQuote, 6000);
-			function randomQuote(){
-			  let random = quotes[Math.floor(Math.random() * quotes.length)];
-			  quotation.innerText = `"${random.quote}"`;
-			  source.innerText = random.source;
-			}
-
 			var i = 0;
 			
 			var speed = 50;
